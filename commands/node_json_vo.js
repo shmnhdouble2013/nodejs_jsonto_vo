078a14c8baa64591ca02e7d0316a8f7944922366
shmnhdouble2013 <huangjia86@yeah.net>
2014-02-26 23:01:26 +0800

    think nodejs

--- commands/node_json_vo.js
+++ commands/node_json_vo.js
@@ -1,4 +1,16 @@
 
+/****
+*
+* 1、引入模块 -- 读取模板 -- 写入模板
+* 2、查找路径 --- 模板路径 、 nodejs路径、 js数据路径、jar工具路径、最终存放文档路径 
+*
+* 3、获取文件流---
+* 4、输入文件流----转换文件流--出错标记
+* 5、存入模板文件流
+* 6、写入模板文件流
+*
+**/
+
 var url = require('url'),
     Modules = require('Modules'),
     path = require('path'),
@@ -17,5 +29,3 @@ var url = require('url'),
 exports.area = function (r) {
     return PI * r * r;
 };
-
-process.
